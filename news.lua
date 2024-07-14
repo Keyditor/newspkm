f04967f5e20a3d22741a885561593e1cb1ffb737
Keyson Correia <wasdvlog@gmail.com>
2024-07-14 15:10:46 -0300

    Create news.lua

---
+++ news.lua
@@ -0,0 +1,41 @@
+-- Portal de Noticias do Servidor
+-- Aplicativo de comunicação e atualização
+
+-- Importando livrarias
+salt = assert(require("salt"))
+basalt = require("basalt")
+
+-- Declarando variaveis vitais
+nodoType = "Cliente"
+nodoId = os.getComputerID()
+nodoOwner = "Test Guy"
+
+-- Inicio do código
+
+-- Tela de Menu
+
+mainMenu = basalt.createFrame()
+    :setTheme({FrameBG = colors.black, FrameFG = colors.lightGray})
+    
+
+
+
+    menuObt = mainMenu
+        :addFrame()
+        :setSize(24, 18)
+        :setPosition(2, 2)
+        
+
+        subTittle = menuObt
+            :addLabel()
+            :setPosition(1,1)
+            :setText("DO SERVIDOR")
+            :show()
+        subTittle = menuObt
+            :addLabel()
+            :setPosition(2,1)
+            :setText("DO SERVIDOR")
+            :show()
+
+-- Roda o programa
+basalt.autoUpdate()

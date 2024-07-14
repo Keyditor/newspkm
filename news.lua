4d513b65dec82f9f3848bc8296408e250b404cfb
Keyditor <keysonfcorreia@gmail.com>
2024-07-14 17:18:48 -0300

    Adicionando botôes e aprimorando graficos

--- news.lua
+++ news.lua
@@ -2,14 +2,19 @@
 -- Aplicativo de comunicação e atualização
 
 -- Importando livrarias
-salt = assert(require("salt.lua"))
-basalt = require("basalt.lua")
+salt = assert(require("salt"))
+basalt = require("basalt")
 
 
 -- Declarando variaveis vitais
 nodoType = "Cliente"
 nodoId = os.getComputerID()
 nodoOwner = "Test Guy"
+
+-- Declarando variaveis de estilo
+
+buttonBg = colors.black
+mainFg = colors.lime
 
 -- Inicio do código
 
@@ -20,25 +25,71 @@ mainMenu = basalt.createFrame()
     
 
 
-
+    -- Frame do Menu
     menuObt = mainMenu
         :addFrame()
+        --:setScrollable()
+        --:setScrollAmount(25)
         :setSize(24, 18)
-        :setForeground(colors.white)
+        :setForeground(mainFg)
         :setPosition(2, 2)
         
-
-        subTittle = menuObt
+        -- Elementos do menu
+        Tittle = menuObt
             :addLabel()
-            :setPosition(1,1)
-            :setText("DO SERVIDOR")
+            :setPosition(4,3)
+            :setText("PORTAL DE NOTICIAS")
             
             :show()
         subTittle = menuObt
             :addLabel()
-            :setPosition(2,1)
-            :setText("DO SERVIDOR")
+            :setPosition(7,4)
+            :setText("DO  SERVIDOR")
             :show()
+
+
+        
+        
+        bntRegras = menuObt
+            
+            :addButton()
+            :setText("REGRAS DO PORTAL")
+            :setBackground(buttonBg)
+            :setPosition(4,8)
+            :setSize(18,3)
+            
+
+        bntFeed  = menuObt
+            :addButton()
+            :setText("FEED DE NOTICIAS")
+            :setBackground(buttonBg)
+            :setPosition(4,12)
+            :setSize(18,3)
+            
+        
+        -- Funções do menu
+        function bntHover(nBnt)
+            if nBnt == 0 then
+                bntRegras:setBackground(mainFg)
+                bntRegras:setForeground(buttonBg)
+            end
+            if nBnt == 1 then
+                bntFeed:setBackground(mainFg)
+                bntFeed:setForeground(buttonBg)
+            end
+            if nBnt == nil then
+                bntRegras:setBackground(buttonBg)
+                bntRegras:setForeground(mainFg)
+                bntFeed:setBackground(buttonBg)
+                bntFeed:setForeground(mainFg)
+            end
+        end
+            
+        -- Configurações dos Elementos
+        bntRegras:onHover(bntHover(0))
+        bntFeed:onHover(bntHover(1))
+        bntRegras:onLeave(bntHover(nil))
+        bntFeed:onLeave(bntHover(nil))
 
 -- Roda o programa
 basalt.autoUpdate()

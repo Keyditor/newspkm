e522a0df4d0335be7d2f637727ab3494541eb5d1
Keyditor <keysonfcorreia@gmail.com>
2024-07-14 18:58:25 -0300

    Botões posicionados e rolagem definida!

--- news.lua
+++ news.lua
@@ -50,63 +50,86 @@ mainMenu = basalt.createFrame()
 
         
         
-            bntRegras = menuObt
+        bntRegras = menuObt
             
-            :addButton()
+            :addButton("0")
             :setText("REGRAS DO PORTAL")
             :setBackground(buttonBg)
             :setForeground(mainFg)
             :setPosition(4,8)
             :setSize(18,3)
+            --.bntId == 0
             
 
         bntFeed  = menuObt
-            :addButton()
+            :addButton("1")
             :setText("FEED DE NOTICIAS")
             :setBackground(buttonBg)
             :setForeground(mainFg)
             :setPosition(4,12)
             :setSize(18,3)
+            --.bntId == 1
 
         bntPost = menuObt
             
-            :addButton()
+            :addButton("2")
             :setText("POSTAR NOTICIA")
             :setBackground(buttonBg)
             :setForeground(mainFg)
             :setPosition(4,16)
             :setSize(18,3)
+            --.bntId == 2
             
 
         bntExit = menuObt
-            :addButton()
-            :setText("EXIT")
+            :addButton("3")
+            :setText("SAIR")
             :setBackground(buttonBg)
             :setForeground(mainFg)
             :setPosition(4,20)
             :setSize(18,3)
+            --.bntId == 3
 
         
     -- Funções do menu
+
     function buttonOnScroll(self, direction, x, y)
         basalt.debug("Someone scrolls on me!")
     end
     function bntHover(self)
         self:setForeground(colors.black)
         self:setBackground(buttonHover)
+        basalt.debug("Opa")
     end
     function bntLeave(self)
         self:setForeground(mainFg)
         self:setBackground(buttonBg)
+        --basalt.debug()
+        
+        if self:getName() == "0" then
+            basalt.debug(" 0 Pressed! ")
+        end
+        if self:getName() == "1" then
+            basalt.debug(" 1 Pressed! ")
+        end
+        if self:getName() == "2" then
+            basalt.debug(" 2 Pressed! ")
+        end
+        if self:getName() == "3" then
+            basalt.debug(" 3 Pressed! ")
+        end
     end
             
     -- Configurações dos Elementos
 
-    
     bntRegras:onClick(bntHover)
-    bntFeed:onClick(bntHover)
     bntRegras:onClickUp(bntLeave)
+    bntFeed:onClick(bntHover)
     bntFeed:onClickUp(bntLeave)
+    bntPost:onClick(bntHover)
+    bntPost:onClickUp(bntLeave)
+    bntExit:onClick(bntHover)
+    bntExit:onClickUp(bntLeave)
 
 -- Roda o programa
 basalt.autoUpdate()
